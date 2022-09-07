c42098968a79a7984c36e40892fdff37720862aa
Sojo C Johny <sojocjohny37@gmail.com>
2022-09-07 21:51:39 +0530

    Added an header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='head'>
+        <div className='header'>
+            <p className='head-info'>Live Chat</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
