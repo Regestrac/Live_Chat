164cc361cc3ad14b88c2551ef60d643b64dce283
Sojo C Johny <sojocjohny37@gmail.com>
2022-09-08 21:47:46 +0530

    Added a section to show the chats

---
+++ src/components/ChatBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ChatBox.css'
+
+const ChatBox = () => {
+  return (
+    <div className='chat-box'>
+        <div className='chats'>
+            <p className='chat-text'>Chat 0</p>
+            <p className='chat-text'>This is Chat 1</p>
+            <p className='chat-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+            <p className='chat-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        </div>
+    </div>
+  )
+}
+
+export default ChatBox
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ChatBox from './ChatBox'
 import Header from './Header'
 
 const Home = () => {
   return (
     <div className='home'>
         <Header />
+        <ChatBox />
     </div>
   )
 }
