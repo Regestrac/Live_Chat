3b4645bdde413d722904f6233f06e4dc05440557
Sojo C Johny <sojocjohny37@gmail.com>
2022-09-10 20:55:21 +0530

    Added auth & form submition

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Form.css'
 
-const Form = () => {
+const Form = ({setIsAuth, setName}) => {
     return (
         <div className='form-box'>
-            <form className='form'>
-                <input className='name-input' autoComplete='off' type='text' required></input>
+            <form className='form' onSubmit={()=>setIsAuth(true)} >
+                <input className='name-input' onChange={(e)=>setName(e.target.value)} autoComplete='off' type='text' required></input>
                 <label className='name-label'><span className='content-name'>Name</span></label>
-                <button className='enter-btn'>Enter Chat</button>
+                <button className='enter-btn' >Enter Chat</button>
             </form>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,21 +4,26 @@ import Base from './Base'
 import ChatBox from './ChatBox'
 import Header from './Header'
 import Form from './Form'
+import { useState } from 'react'
 
 const Home = () => {
+  const [isAuth, setIsAuth] = useState(false)
+  const [name, setName] = useState("")
+  console.log(name)
   return (
     <div className='home'>
       <div>
         <div className='head-div'>
-        <Header />
+          <Header />
         </div>
-        <div className='main'>
-        <ChatBox />
-        <Base />
+        <div className={!isAuth && "blur" } >
+          <ChatBox name={name} />
+          <Base />
         </div>
+       {!isAuth &&
         <div className='form-div'>
-        <Form />
-        </div>
+          <Form setIsAuth={setIsAuth} setName={setName} />
+        </div>}
       </div>
     </div>
   )
